280ad8601f80e0d0b4a4da0a83db9b8492809a92
jonghyouk yun <ageldama@gmail.com>
2025-03-03 19:23:35 +0900

    --wip-- [skip ci]

--- src/sdl2-im-test.c
+++ src/sdl2-im-test.c
@@ -1,49 +1,81 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
 #include <SDL.h>
 
 typedef struct {
-  SDL_Window* window;
+  SDL_Window* win;
+  SDL_Surface* win_surface;
 } App;
 
+App app;
 
-int main(int argc, char *argv[])
+void cleanup()
+{
+  if (app.win_surface) {
+    SDL_DestroyWindowSurface(app.win);
+    app.win_surface = NULL;
+  }
+
+  if (app.win) {
+    SDL_DestroyWindow(app.win);
+    app.win = NULL;
+  }
+
+  SDL_Quit();
+}
+
+void init()
 {
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
     fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-    return 1;
+    exit(EXIT_FAILURE);
+  }
+}
+
+void prepare()
+{
+  /* Create our window */
+  app.win = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
+
+  /* Make sure creating the window succeeded */
+  if (!app.win) {
+    fprintf(stderr, "Error creating window: %s", SDL_GetError());
+    exit(EXIT_FAILURE);
   }
 
-  // Create our window
-  SDL_Window* window;
-  window = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
+  /* Get the surface from the window */
+  app.win_surface = SDL_GetWindowSurface( app.win );
 
-  // Make sure creating the window succeeded
-  if ( !window ) {
-    /* TODO cout << "Error creating window: " << SDL_GetError()  << endl; */
-    return 1;
+  /* Make sure getting the surface succeeded */
+  if ( !app.win_surface ) {
+    fprintf(stderr, "Error getting surface: %s", SDL_GetError());
+    exit(EXIT_FAILURE);
+  }
+}
+
+void redraw()
+{
+  /* Fill the window with a white rectangle */
+  SDL_FillRect( app.win_surface, NULL, SDL_MapRGB( app.win_surface->format, 255, 255, 255 ) );
+
+  /* Update the window display */
+  SDL_UpdateWindowSurface( app.win );
+}
+
+int main(int argc, char *argv[])
+{
+  memset(&app, 0, sizeof(app));
+
+  init();
+  atexit(cleanup);
+
+  prepare();
+
+  while(1) {
+    redraw();
+    SDL_Delay(10);
   }
 
-  // Get the surface from the window
-  SDL_Surface* win_surface;
-  win_surface = SDL_GetWindowSurface( window );
-
-  // Make sure getting the surface succeeded
-  if ( !win_surface ) {
-    /* TODO cout << "Error getting surface: " << SDL_GetError() << endl; */
-    // End the program
-    return 1;
-  }
-
-  // Fill the window with a white rectangle
-  SDL_FillRect( win_surface, NULL, SDL_MapRGB( win_surface->format, 255, 255, 255 ) );
-
-  // Update the window display
-  SDL_UpdateWindowSurface( window );
-
-  // Destroy the window. This will also destroy the surface
-  SDL_DestroyWindow( window );
-	
-  SDL_Quit();
-
-  return 0;
+  exit(EXIT_SUCCESS);
 }
