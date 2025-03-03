7cba9acf4f95fb84ca33e2d0d0f38be9f8254235
jonghyouk yun <ageldama@gmail.com>
2025-03-03 19:29:38 +0900

    hello

--- src/sdl2-im-test.c
+++ src/sdl2-im-test.c
@@ -4,78 +4,87 @@
 #include <SDL.h>
 
 typedef struct {
-  SDL_Window* win;
-  SDL_Surface* win_surface;
+    SDL_Window* win;
+    SDL_Surface* win_surface;
 } App;
 
 App app;
 
 void cleanup()
 {
-  if (app.win_surface) {
-    SDL_DestroyWindowSurface(app.win);
-    app.win_surface = NULL;
-  }
+    if (app.win_surface) {
+        SDL_DestroyWindowSurface(app.win);
+        app.win_surface = NULL;
+    }
 
-  if (app.win) {
-    SDL_DestroyWindow(app.win);
-    app.win = NULL;
-  }
+    if (app.win) {
+        SDL_DestroyWindow(app.win);
+        app.win = NULL;
+    }
 
-  SDL_Quit();
+    SDL_Quit();
 }
 
 void init()
 {
-  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-    fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        fprintf(stderr, "Error initializing SDL: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
 }
 
 void prepare()
 {
-  /* Create our window */
-  app.win = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
+    /* Create our window */
+    app.win = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
 
-  /* Make sure creating the window succeeded */
-  if (!app.win) {
-    fprintf(stderr, "Error creating window: %s", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
+    /* Make sure creating the window succeeded */
+    if (!app.win) {
+        fprintf(stderr, "Error creating window: %s", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
 
-  /* Get the surface from the window */
-  app.win_surface = SDL_GetWindowSurface( app.win );
+    /* Get the surface from the window */
+    app.win_surface = SDL_GetWindowSurface( app.win );
 
-  /* Make sure getting the surface succeeded */
-  if ( !app.win_surface ) {
-    fprintf(stderr, "Error getting surface: %s", SDL_GetError());
-    exit(EXIT_FAILURE);
-  }
+    /* Make sure getting the surface succeeded */
+    if ( !app.win_surface ) {
+        fprintf(stderr, "Error getting surface: %s", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
 }
 
 void redraw()
 {
-  /* Fill the window with a white rectangle */
-  SDL_FillRect( app.win_surface, NULL, SDL_MapRGB( app.win_surface->format, 255, 255, 255 ) );
+    /* Fill the window with a white rectangle */
+    SDL_FillRect( app.win_surface, NULL, SDL_MapRGB( app.win_surface->format, 0x90, 0x00, 0x00 ) );
 
-  /* Update the window display */
-  SDL_UpdateWindowSurface( app.win );
+    /* Update the window display */
+    SDL_UpdateWindowSurface( app.win );
 }
 
 int main(int argc, char *argv[])
 {
-  memset(&app, 0, sizeof(app));
+    memset(&app, 0, sizeof(app));
 
-  init();
-  atexit(cleanup);
+    init();
+    atexit(cleanup);
 
-  prepare();
+    prepare();
 
-  while(1) {
-    redraw();
-    SDL_Delay(10);
-  }
+    int pls_exit = 0;
+    SDL_Event event;
+    while (!pls_exit) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+            case SDL_QUIT:
+                pls_exit = 1;
+                break;
+            }
 
-  exit(EXIT_SUCCESS);
+            redraw();
+        }
+    }
+
+    exit(EXIT_SUCCESS);
 }
