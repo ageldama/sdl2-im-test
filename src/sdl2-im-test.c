c1b90f92a0260e432de083887a83ffbbad050dc2
jonghyouk yun <ageldama@gmail.com>
2025-03-03 19:10:03 +0900

    hello

--- src/sdl2-im-test.c
+++ src/sdl2-im-test.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <SDL.h>
 
+typedef struct {
+  SDL_Window* window;
+} App;
+
 
 int main(int argc, char *argv[])
 {
@@ -9,6 +13,36 @@ int main(int argc, char *argv[])
     return 1;
   }
 
+  // Create our window
+  SDL_Window* window;
+  window = SDL_CreateWindow( "Example", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280, 720, SDL_WINDOW_SHOWN );
+
+  // Make sure creating the window succeeded
+  if ( !window ) {
+    /* TODO cout << "Error creating window: " << SDL_GetError()  << endl; */
+    return 1;
+  }
+
+  // Get the surface from the window
+  SDL_Surface* win_surface;
+  win_surface = SDL_GetWindowSurface( window );
+
+  // Make sure getting the surface succeeded
+  if ( !win_surface ) {
+    /* TODO cout << "Error getting surface: " << SDL_GetError() << endl; */
+    // End the program
+    return 1;
+  }
+
+  // Fill the window with a white rectangle
+  SDL_FillRect( win_surface, NULL, SDL_MapRGB( win_surface->format, 255, 255, 255 ) );
+
+  // Update the window display
+  SDL_UpdateWindowSurface( window );
+
+  // Destroy the window. This will also destroy the surface
+  SDL_DestroyWindow( window );
+	
   SDL_Quit();
 
   return 0;
